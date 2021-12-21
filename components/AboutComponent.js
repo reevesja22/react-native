d88bfaa25ed66f373e827b962b14e5e12a50d6f0
JakeReeves <reevesja01@gmail.com>
2021-12-20 22:38:29 -0600

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { View, ScrollView, FlatList } from 'react-native';
 import { Text, Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -45,17 +46,41 @@ class About extends Component {
             );
         };
 
-
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                <Mission />
+                <Card
+                    title="Community Partners">
+                    <Loading />
+                </Card>
+            </ScrollView>
+            );
+        }
+        if (this.props.partners.errMess) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title="Community Partners">
+                        <Text>{this.props.partners.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
         return (
             <ScrollView>
                 <Mission />
-                <FlatList
-                data={this.props.partners.partners}
-                renderItem={renderPartner}
-                keyExtractor={item => item.id.toString()}
-            />
+                <Card
+                    title="Community Partners">
+                    <FlatList
+                    data={this.props.partners.partners}
+                    renderItem={renderPartner}
+                    keyExtractor={item => item.id.toString()}
+                />
+                </Card>
             </ScrollView>
-        )
+        );
     }
 }
 
