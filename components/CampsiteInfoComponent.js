caba79c8ee849909dc331225f9b74378de448099
JakeReeves <reevesja01@gmail.com>
2022-01-06 23:18:00 -0600

    Week 3 assignment

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -28,7 +28,9 @@ function RenderCampsite(props) {
 
     const view = React.createRef();
 
-    const recognizeDrag = ({dx}) => (dx < 200) ? true : false;
+    const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
+
+    const recognizeComment = ({dx}) => (dx > 200) ? true: false;
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
@@ -49,13 +51,16 @@ function RenderCampsite(props) {
                             onPress: () => console.log("Cancel Pressed")
                         },
                         {
-                            test: "OK",
+                            text: "OK",
                             onPress: () => props.favorite ?
                                 console.log("Already set as a favorite") : props.markFavorite()
                         }
                     ],
                     { cancelable: false }
                 );
+            }
+            else if (recognizeComment(gestureState)) {
+                props.onShowModal();
             }
             return true;
         }
